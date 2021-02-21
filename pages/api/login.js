f58483b136aaab95bbdc7fce3c52206ba66dc21c
Cole87 <zbni.rs@gmail.com>
2021-02-21 08:13:45 +0100

    1) httpOnly Cookie resolved

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,22 +1,18 @@
 const cookie = require('cookie');
-const axios = require('axios');
+import { getCookie } from './getCookie';
 
 export default async (req, res) => {
 	const userName = req.body.userData.userName;
 	const password = req.body.userData.password;
 	const url = `http://localhost:5000/users/login?username=${userName}&password=${password}`;
 
-	const getCookie = await axios({ method: 'get', url: url })
-		.then(response => {
-			return response.data.access_token; //vraca cookie sa tvog servera i zelim da ga koristim u res.setHeader kako bih poslao cookie klijentu
-		})
-		.then(error => {
-			console.log(error);
-		});
+	const token = await getCookie({ method: 'get', url: url }).then(response => {
+		return response.data.access_token;
+	});
 
 	const request_config = res.setHeader(
 		'Set-Cookie',
-		cookie.serialize('token', getCookie, {
+		cookie.serialize('token', token, {
 			/* ovde vraca undefined */ httpOnly: true,
 			secure: process.env.NODE_ENV !== 'development',
 			maxAge: 60 * 60,
