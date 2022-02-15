46dc5652ed260ee1feabd2233fe85a5eed704fed
DESKTOP-8J89SS4\sox-darko <soxdarko@gmail.com>
2022-02-15 12:50:30 +0100

    Dodao eslint pravila

--- pages/api/getCompanyGuideStatus.js
+++ pages/api/getCompanyGuideStatus.js
@@ -1,24 +1,24 @@
 import { fetchJson } from '../../api/fetchJson';
 
 export default async (req, res) => {
-	const cookie = req.headers.cookie;
-	const token = cookie.substring(cookie.indexOf('=') + 1);
-	const url = `users/getCompanyGuideStatus`;
+    const cookie = req.headers.cookie;
+    const token = cookie.substring(cookie.indexOf('=') + 1);
+    const url = `users/getCompanyGuideStatus`;
 
-	async function getCompanyGuideStatus() {
-		const api = await fetchJson(url, 'get', token)
-			.then(res => {
-				return res.data;
-			})
-			.catch(err => {
-				console.log(err);
-			});
+    async function getCompanyGuideStatus() {
+        const api = await fetchJson(url, 'get', token)
+            .then((res) => {
+                return res.data;
+            })
+            .catch((err) => {
+                console.log(err);
+            });
 
-		return api;
-	}
+        return api;
+    }
 
-	const data = getCompanyGuideStatus();
+    const data = getCompanyGuideStatus();
 
-	res.statusCode = 200;
-	res.json(await data);
+    res.statusCode = 200;
+    res.json(await data);
 };
